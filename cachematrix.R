269bc34a6fa776d4c85ad65a4830d4f9ae58d47d
jileparducho <vina.parducho@gmail.com>
2015-03-22 22:45:52 +0800

    Update cachematrix.R
    This is in partial fulfillment of the requirements needed in the "Programming Assignment 2: Lexical Scoping".

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,81 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This code is in partial fulfillment of the requirements in "Programming 2: Lexical
+## Scoping" of the "R Programming" course in Coursera.
 
-## Write a short comment describing this function
+## The goal of the following pair of functions is to cache the inverse of a matrix.
+
+## Functions were created based on the given example, caching the mean of a vector.
+## In this example we introduce the <<- operator which can be used to assign a value 
+## to an object in an environment that is different from the current environment.
+## Below are two functions that are used to create a special object that stores a 
+## numeric vector and cache's its mean.
+
+## The first function, makeVector creates a special "vector", which is really a list 
+## containing a function to
+        ## 1. set the value of the vector
+        ## 2. get the value of the vector
+        ## 3. set the value of the mean
+        ## 4. get the value of the mean
+## makeVector <- function(x = numeric()) {
+        ## m <- NULL
+        ## set <- function(y) {
+                 ##  x <<- y
+                 ##  m <<- NULL
+        ## }
+        ## get <- function() x
+        ## setmean <- function(mean) m <<- mean
+        ## getmean <- function() m
+        ## list(set = set, get = get,
+             ## setmean = setmean,
+             ## getmean = getmean)
+## }
+
+## The following function calculates the mean of the special "vector" created with 
+## the above function. However, it first checks to see if the mean has already been 
+## calculated. If so, it gets the mean from the cache and skips the computation. 
+## Otherwise, it calculates the mean of the data and sets the value of the mean in 
+## the cache via the setmean function.
+
+## cachemean <- function(x, ...) {
+        ## m <- x$getmean()
+        ## if(!is.null(m)) {
+                ## message("getting cached data")
+                ## return(m)
+        ## }
+        ## data <- x$get()
+        ## m <- mean(data, ...)
+        ## x$setmean(m)
+        ## m
+## }
+
+## Moving forward with the assignment, the function below creates a special "matrix" 
+## object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## The function below computes the inverse of the special "matrix" returned by the makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve 
+## should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Returns a matrix that is the inverse of 'x'.
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  matrix <- x$get()
+  m <- solve(matrix, ...)
+  x$setinverse(m)
+  m
 }
+
